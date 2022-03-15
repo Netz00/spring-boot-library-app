9f3b82f4c254890bf9db2643dc78de96edb248cc
Bozo <bdurd00@gmail.com>
2022-03-15 23:36:59 +0100

    test data added for two tables, jpa and h2 configuration fix

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO author
+(id, name,family_name,birth_year,death_year,genre,note)
+VALUES
+(1, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(2, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(3, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(4, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(5, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(6, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(7, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(8, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(9, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...'),
+(10, 'Vlado','Nazor','1965','2022','proza','poznati pisac ...');
+
+
+INSERT INTO user
+(id, name, family_name, date_birth, date_joining, email, mobile, address, note, last_modified_date)
+VALUES
+(1, 'Vlado', 'Nazor', '2022-03-15', '2022-03-15', 'pruioz@pporooza.hr', '+385 254 2548 154', 'Zagreb', 'nekakav zapis', '2022-03-15'),
+(2, 'Vlado', 'Nazor', '2022-03-15', '2022-03-15', 'proz@prooza.hr', '+385 254 2548 154', 'Zagreb', 'nekakav zapis', '2022-03-15'),
+(3, 'Vlado', 'Nazor', '2022-03-15', '2022-03-15', 'prozl@prozoia.hr', '+385 254 2548 154', 'Zagreb', 'nekakav zapis', '2022-03-15'),
+(4, 'Vlado', 'Nazor', '2022-03-15', '2022-03-15', 'prozi@prozoa.hr', '+385 254 2548 154', 'Zagreb', 'nekakav zapis', '2022-03-15'),
+(5, 'Vlado', 'Nazor', '2022-03-15', '2022-03-15', 'proopz@priiuoza.hr', '+385 254 2548 154', 'Zagreb', 'nekakav zapis', '2022-03-15'),
+(6, 'Vlado', 'Nazor', '2022-03-15', '2022-03-15', 'propz@oprouza.hr', '+385 254 2548 154', 'Zagreb', 'nekakav zapis', '2022-03-15'),
+(7, 'Vlado', 'Nazor', '2022-03-15', '2022-03-15', 'przoz@prozoa.hr', '+385 254 2548 154', 'Zagreb', 'nekakav zapis', '2022-03-15');
