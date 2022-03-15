2dd9b9315907c1f8432fde0261abda7f6f4685d0
Bozo <bdurd00@gmail.com>
2022-03-15 21:01:06 +0100

    Spring boot project generated

---
+++ src/test/java/com/netz00/libraryapp/LibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netz00.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
