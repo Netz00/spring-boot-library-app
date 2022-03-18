17cc99b82717863f2fcf574a666c3ef9784fe7b8
Bozo <bdurd00@gmail.com>
2022-03-18 21:52:28 +0100

    User domain model validations added

--- src/main/java/com/netz00/libraryapp/domain/User.java
+++ src/main/java/com/netz00/libraryapp/domain/User.java
@@ -1,95 +1,72 @@
 package com.netz00.libraryapp.domain;
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import java.sql.Timestamp;
 import java.time.LocalDate;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity
-@Table(
-        name = "user",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "user_email_unique", columnNames = "email")
-        })
+@Table(name = "user", uniqueConstraints = {@UniqueConstraint(name = "user_email_unique", columnNames = "email")})
 public class User {
 
     @Id
-    @SequenceGenerator(
-            name = "user_sequence",
-            sequenceName = "user_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = SEQUENCE,
-            generator = "user_sequence"
-    )
-    @Column(
-            name = "id",
-            updatable = false
-    )
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = SEQUENCE, generator = "user_sequence")
+    @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(
-            name = "name",
-            nullable = false
-    )
+    @Size(min = 1, max = 50, message = "Name must be between 1 and 50 characters")
+    @NotEmpty(message = "Name is required. Name cannot be null or empty")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(
-            name = "family_name",
-            nullable = false
-    )
+    @Size(min = 1, max = 50, message = "Family name must be between 1 and 50 characters")
+    @NotEmpty(message = "Family name is required. Family name cannot be null or empty")
+    @Column(name = "family_name", nullable = false)
     private String family_name;
 
-    @Column(
-            name = "date_birth",
-            nullable = false
-    )
+    @JsonFormat(pattern="yyyy-MM-dd")
+    @Column(name = "date_birth", nullable = false)
     private LocalDate date_birth;
 
-    @Column(
-            name = "date_joining",
-            nullable = false
-    )
-    private LocalDate date_joining;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name = "date_joining", nullable = false)
+    private Timestamp date_joining;
 
-    @Column(
-            name = "email",
-            nullable = false
-    )
+    @Size(min = 3, max = 50, message = "Email must be between 3 and 50 characters")
+    @NotEmpty(message = "Email is required. Email cannot be null or empty")
+    @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(
-            name = "mobile",
-            nullable = false
-    )
+    @Size(min = 1, max = 50, message = "Mobile must be between 1 and 50 characters")
+    @NotEmpty(message = "Mobile is required. Mobile cannot be null or empty")
+    @Column(name = "mobile", nullable = false)
     private String mobile;
 
-    @Column(
-            name = "address",
-            nullable = false
-    )
+    @Size(min = 1, max = 50, message = "Address must be between 1 and 50 characters")
+    @NotEmpty(message = "Address is required. Address cannot be null or empty")
+    @Column(name = "address", nullable = false)
     private String address;
 
-    @Column(
-            name = "note",
-            nullable = true,
-            columnDefinition = "TEXT"
-    )
+    @Size(min = 0, max = 200, message = "Note must be up to 200 characters max")
+    @Column(name = "note", nullable = true, columnDefinition = "TEXT")
     private String note;
 
-    @Column(
-            name = "last_modified_date",
-            nullable = false
-    )
-    private LocalDate last_modified_date;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name = "last_modified_date", nullable = true)
+    private Timestamp last_modified_date;
 
 
     public User() {
     }
 
-    public User(String name, String family_name, LocalDate date_birth, LocalDate date_joining, String email, String mobile, String address, String note, LocalDate last_modified_date) {
+    public User(String name, String family_name, LocalDate date_birth, Timestamp date_joining, String email, String mobile, String address, String note, Timestamp last_modified_date) {
         this.name = name;
         this.family_name = family_name;
         this.date_birth = date_birth;
@@ -130,11 +107,11 @@ public class User {
         this.date_birth = date_birth;
     }
 
-    public LocalDate getDate_joining() {
+    public Timestamp getDate_joining() {
         return date_joining;
     }
 
-    public void setDate_joining(LocalDate date_joining) {
+    public void setDate_joining(Timestamp date_joining) {
         this.date_joining = date_joining;
     }
 
@@ -170,11 +147,11 @@ public class User {
         this.note = note;
     }
 
-    public LocalDate getLast_modified_date() {
+    public Timestamp getLast_modified_date() {
         return last_modified_date;
     }
 
-    public void setLast_modified_date(LocalDate last_modified_date) {
+    public void setLast_modified_date(Timestamp last_modified_date) {
         this.last_modified_date = last_modified_date;
     }
 }
