5f26e1414c8696bc2d879ae7b047ad9021d443c8
Bozo <bdurd00@gmail.com>
2022-03-18 21:36:18 +0100

    Lending domain model validations added

--- src/main/java/com/netz00/libraryapp/domain/Lending.java
+++ src/main/java/com/netz00/libraryapp/domain/Lending.java
@@ -1,10 +1,13 @@
 package com.netz00.libraryapp.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.netz00.libraryapp.domain.enumeration.LendingStatus;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
-import java.time.LocalDate;
+import java.sql.Timestamp;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
@@ -12,77 +15,63 @@ import static javax.persistence.GenerationType.SEQUENCE;
  * ManyToMany Join table for User - Book relationship
  */
 @Entity
-@Table(
-        name = "lending"
-)
+@Table(name = "lending")
 public class Lending {
 
     @Id
-    @SequenceGenerator(
-            name = "lending_sequence",
-            sequenceName = "lending_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = SEQUENCE,
-            generator = "lending_sequence"
-    )
-    @Column(
-            name = "id",
-            updatable = false
-    )
+    @SequenceGenerator(name = "lending_sequence", sequenceName = "lending_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = SEQUENCE, generator = "lending_sequence")
+    @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(
-            name = "date_lending",
-            nullable = false
-    )
-    private LocalDate date_lending;
+    /**
+     * Timestamp when Book was lended.
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name = "date_lending", nullable = false)
+    private Timestamp date_lending;
 
-    @Column(
-            name = "date_returning",
-            nullable = true
-    )
-    private LocalDate date_returning;
-    
+    /**
+     * Timestamp when Book was returned.
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name = "date_returning", nullable = true)
+    private Timestamp date_returning;
+
+    /**
+     * Status of this Lending.
+     */
+    @NotEmpty(message = "Status is required. Status cannot be null or empty")
     @Enumerated(EnumType.STRING)
-    @Column(
-            name = "status",
-            nullable = false
-    )
+    @Column(name = "status", nullable = false)
     private LendingStatus status;
 
-    @Column(
-            name = "note",
-            nullable = true,
-            columnDefinition = "TEXT"
-    )
+    /**
+     * Note about this Lending.
+     */
+    @Size(min = 0, max = 200, message = "Note must be up to 200 characters max")
+    @Column(name = "note", nullable = true, columnDefinition = "TEXT")
     private String note;
 
-    @Column(
-            name = "last_modified_date",
-            nullable = false
-    )
-    private LocalDate last_modified_date;
+    /**
+     * Timestamp when this Lending was last modified.
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name = "last_modified_date", nullable = true)
+    private Timestamp last_modified_date;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(
-            name = "user_id",
-            referencedColumnName = "id"
-    )
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     User user_id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(
-            name = "book_id",
-            referencedColumnName = "id"
-    )
+    @JoinColumn(name = "book_id", referencedColumnName = "id")
     Book book_id;
 
     public Lending() {
     }
 
-    public Lending(LocalDate date_lending, LocalDate date_returning, LendingStatus status, String note, LocalDate last_modified_date, User user_id, Book book_id) {
+    public Lending(Timestamp date_lending, Timestamp date_returning, LendingStatus status, String note, Timestamp last_modified_date, User user_id, Book book_id) {
         this.date_lending = date_lending;
         this.date_returning = date_returning;
         this.status = status;
@@ -96,19 +85,19 @@ public class Lending {
         return id;
     }
 
-    public LocalDate getDate_lending() {
+    public Timestamp getDate_lending() {
         return date_lending;
     }
 
-    public void setDate_lending(LocalDate date_lending) {
+    public void setDate_lending(Timestamp date_lending) {
         this.date_lending = date_lending;
     }
 
-    public LocalDate getDate_returning() {
+    public Timestamp getDate_returning() {
         return date_returning;
     }
 
-    public void setDate_returning(LocalDate date_returning) {
+    public void setDate_returning(Timestamp date_returning) {
         this.date_returning = date_returning;
     }
 
@@ -128,11 +117,11 @@ public class Lending {
         this.note = note;
     }
 
-    public LocalDate getLast_modified_date() {
+    public Timestamp getLast_modified_date() {
         return last_modified_date;
     }
 
-    public void setLast_modified_date(LocalDate last_modified_date) {
+    public void setLast_modified_date(Timestamp last_modified_date) {
         this.last_modified_date = last_modified_date;
     }
 
