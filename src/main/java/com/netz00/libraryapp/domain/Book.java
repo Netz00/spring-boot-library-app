bade55c69d03122abcf7a17a81b2d7fa05a3279d
Bozo <bdurd00@gmail.com>
2022-03-18 21:52:11 +0100

    Book domain model validations added

--- src/main/java/com/netz00/libraryapp/domain/Book.java
+++ src/main/java/com/netz00/libraryapp/domain/Book.java
@@ -1,36 +1,25 @@
 package com.netz00.libraryapp.domain;
 
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-import java.time.LocalDate;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import java.sql.Timestamp;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity
-@Table(
-        name = "book",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "book_isbn_unique", columnNames = "isbn")
-        })
+@Table(name = "book", uniqueConstraints = {@UniqueConstraint(name = "book_isbn_unique", columnNames = "isbn")})
 public class Book {
 
     @Id
-    @SequenceGenerator(
-            name = "book_sequence",
-            sequenceName = "book_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = SEQUENCE,
-            generator = "book_sequence"
-    )
-    @Column(
-            name = "id",
-            updatable = false
-    )
+    @SequenceGenerator(name = "book_sequence", sequenceName = "book_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = SEQUENCE, generator = "book_sequence")
+    @Column(name = "id", updatable = false)
     private String id;
-
 
     /**
      * TODO
@@ -38,63 +27,60 @@ public class Book {
      * the database so that it will fit in 10 or 13 characters,
      * and to make search/querying easier
      */
-    @Column(
-            name = "isbn",
-            nullable = false,
-            columnDefinition = "CHAR(17)"
-    )
+    @Size(min = 10, max = 13, message = "ISBN must be be between 10 and 13 characters")
+    @NotEmpty(message = "ISBN is required. ISBN cannot be null or empty")
+    @Column(name = "isbn", nullable = false, columnDefinition = "CHAR(17)")
     private String isbn;
 
-    @Column(
-            name = "name",
-            nullable = false
-    )
+    @Size(min = 1, max = 50, message = "Name must be between 1 and 50 characters")
+    @NotEmpty(message = "Name is required. Name cannot be null or empty")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(
-            name = "publisher",
-            nullable = false
-    )
+    /**
+     * Publisher of this Book.
+     */
+    @Size(min = 0, max = 50, message = "Publisher must be be up to 50 characters max")
+    @Column(name = "publisher", nullable = true)
     private String publisher;
 
-    @Column(
-            name = "year",
-            nullable = false
-    )
+    /**
+     * Year of publishing this Book.
+     */
+    @Column(name = "year", nullable = true)
     private Integer year;
 
-    @Column(
-            name = "note",
-            nullable = true,
-            columnDefinition = "TEXT"
-    )
+    /**
+     * Note about this Book.
+     */
+    @Size(min = 0, max = 200, message = "Note must be up to 200 characters max")
+    @Column(name = "note", nullable = true, columnDefinition = "TEXT")
     private String note;
 
-    @Column(
-            name = "created_date",
-            nullable = false
-    )
-    private LocalDate created_date;
+    /**
+     * Timestamp when this Book was inserted.
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name = "created_date", nullable = false)
+    private Timestamp created_date;
 
-    @Column(
-            name = "last_modified_date",
-            nullable = false
-    )
-    private LocalDate last_modified_date;
+    /**
+     * Timestamp when this Book was last modified.
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")
+    @Column(name = "last_modified_date", nullable = true)
+    private Timestamp last_modified_date;
 
 
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(
-            name = "author_id",
-            referencedColumnName = "id"
-    )
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
     private Author author;
 
 
     public Book() {
     }
 
-    public Book(String isbn, String name, String publisher, Integer year, String note, LocalDate created_date, LocalDate last_modified_date, Author author) {
+    public Book(String isbn, String name, String publisher, Integer year, String note, Timestamp created_date, Timestamp last_modified_date, Author author) {
         this.isbn = isbn;
         this.name = name;
         this.publisher = publisher;
@@ -149,19 +135,19 @@ public class Book {
         this.note = note;
     }
 
-    public LocalDate getCreated_date() {
+    public Timestamp getCreated_date() {
         return created_date;
     }
 
-    public void setCreated_date(LocalDate created_date) {
+    public void setCreated_date(Timestamp created_date) {
         this.created_date = created_date;
     }
 
-    public LocalDate getLast_modified_date() {
+    public Timestamp getLast_modified_date() {
         return last_modified_date;
     }
 
-    public void setLast_modified_date(LocalDate last_modified_date) {
+    public void setLast_modified_date(Timestamp last_modified_date) {
         this.last_modified_date = last_modified_date;
     }
 
@@ -175,16 +161,6 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book{" +
-                "id='" + id + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", name='" + name + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", year=" + year +
-                ", note='" + note + '\'' +
-                ", createdDate=" + created_date +
-                ", lastModifiedDate=" + last_modified_date +
-                ", author=" + author +
-                '}';
+        return "Book{" + "id='" + id + '\'' + ", isbn='" + isbn + '\'' + ", name='" + name + '\'' + ", publisher='" + publisher + '\'' + ", year=" + year + ", note='" + note + '\'' + ", createdDate=" + created_date + ", lastModifiedDate=" + last_modified_date + ", author=" + author + '}';
     }
 }
